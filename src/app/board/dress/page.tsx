70b00cd445e136d1bc7c0d8ac875446757f680f4
sneed-ay <info@sneed.jp>
2026-04-03 17:18:35 +0900

    feat: add dress rental board category page

---
+++ src/app/board/dress/page.tsx
@@ -0,0 +1,5 @@
+import BoardCategoryPage from "@/components/BoardCategoryPage";
+
+export default function DressBoardPage() {
+  return <BoardCategoryPage category="dress" categoryLabel="ドレスレンタル" />;
+}
